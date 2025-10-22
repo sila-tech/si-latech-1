7703a8d480da8f60706d2c38b0a8777e9965ee3f
Simon Maina <simonmaina4927@gmail.com>
2025-10-22 07:24:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "inner-bot-461108-c7",
+  "appId": "1:809706983356:web:7d37ac78c83b26e47ca5b9",
+  "apiKey": "AIzaSyBvWiA3xryMDw2k0kc-6qH1DVNdrj8Ps2I",
+  "authDomain": "inner-bot-461108-c7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "809706983356"
+};
