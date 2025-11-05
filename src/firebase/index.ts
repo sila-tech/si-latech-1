66be64d91d0f31aa47a0995c803ea8d807dc13a9
Simon Maina <simonmaina4927@gmail.com>
2025-11-05 09:36:16 +0000

    is these project properly intergrated with the console? because i guess

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -10,7 +10,7 @@ export function initializeFirebase() {
     "projectId": "si-latech",
     "appId": "1:895963925352:web:a074c3e80a083696d11361",
     "storageBucket": "si-latech.appspot.com",
-    "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    "apiKey": "AIzaSyBeo-2gQASzxMOi50bOIVN3aBHGzHYo098",
     "authDomain": "si-latech.firebaseapp.com",
     "messagingSenderId": "895963925352",
     "measurementId": "G-L5V81G2N7N"
